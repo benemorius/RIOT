b5f35e40c2ad1b2d49e2b621a04a316a21dfe9f7
Thomas Stilwell <stilwellt@openlabs.co>
2016-05-18 13:48:44 -0500

    runs

--- boards/srf06-cc26xx/board.c
+++ boards/srf06-cc26xx/board.c
@@ -1,9 +1,26 @@
 /*
- * Copyright (C) 2014 Freie Universität Berlin
+ *    Copyright (c) 2016 Thomas Stilwell <stilwellt@openlabs.co>
  *
- * This file is subject to the terms and conditions of the GNU Lesser General
- * Public License v2.1. See the file LICENSE in the top level directory for more
- * details.
+ *    Permission is hereby granted, free of charge, to any person
+ *    obtaining a copy of this software and associated documentation
+ *    files (the "Software"), to deal in the Software without
+ *    restriction, including without limitation the rights to use,
+ *    copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *    copies of the Software, and to permit persons to whom the
+ *    Software is furnished to do so, subject to the following
+ *    conditions:
+ *
+ *    The above copyright notice and this permission notice shall be
+ *    included in all copies or substantial portions of the Software.
+ *
+ *    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ *    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ *    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ *    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ *    OTHER DEALINGS IN THE SOFTWARE.
  */
 
 /**
@@ -20,39 +37,15 @@
  */
 
 #include "board.h"
-#include "cpu.h"
-
-#include "stdio.h"
-
-static void leds_init(void);
 
 void board_init(void)
 {
-// 	SystemInit();
-
-
     // measure current to check whether execution reaches here and sleeps
-    // it doesn't :(
+    // it does :)
     while(1) {
         __WFI();
     }
 
-    /* initialize the CPU */
     cpu_init();
 
-    /* initialize the boards LEDs */
-    leds_init();
-
-
 }
-
-/**
- * @brief Initialize the boards on-board LEDs
- *
- */
-static void leds_init(void)
-{
-
-}
-
-
