6535c1b0b682851686853d7042858359dc331805
Thomas Stilwell <stilwellt@openlabs.co>
2016-05-19 18:43:40 -0500

    interrupts, uart receive, shell working

--- cpu/cc26xx/vectors.c
+++ cpu/cc26xx/vectors.c
@@ -1,19 +1,37 @@
 /*
- * Copyright (C) 2014-2015 Freie Universität Berlin
+ *    Copyright (c) 2016 Thomas Stilwell <stilwellt@openlabs.co>
  *
- * This file is subject to the terms and conditions of the GNU Lesser General
- * Public License v2.1. See the file LICENSE in the top level directory for more
- * details.
+ *    Permission is hereby granted, free of charge, to any person
+ *    obtaining a copy of this software and associated documentation
+ *    files (the "Software"), to deal in the Software without
+ *    restriction, including without limitation the rights to use,
+ *    copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *    copies of the Software, and to permit persons to whom the
+ *    Software is furnished to do so, subject to the following
+ *    conditions:
+ *
+ *    The above copyright notice and this permission notice shall be
+ *    included in all copies or substantial portions of the Software.
+ *
+ *    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ *    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ *    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ *    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ *    OTHER DEALINGS IN THE SOFTWARE.
  */
 
+
 /**
- * @ingroup     cpu_stm32f3
+ * @ingroup     cpu_cc26xx
  * @{
  *
  * @file
  * @brief       Interrupt vector definitions
  *
- * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
+ * @author      Thomas Stilwell <stilwellt@openlabs.co>
  *
  * @}
  */
@@ -34,73 +52,45 @@ void dummy_handler(void) {
 WEAK_DEFAULT void isr_svc(void);
 WEAK_DEFAULT void isr_pendsv(void);
 WEAK_DEFAULT void isr_systick(void);
-/* STM32F3 specific interrupt vectors */
-WEAK_DEFAULT void isr_wwdg(void);
-WEAK_DEFAULT void isr_pvd(void);
-WEAK_DEFAULT void isr_tamp_stamp(void);
-WEAK_DEFAULT void isr_rtc_wkup(void);
-WEAK_DEFAULT void isr_flash(void);
-WEAK_DEFAULT void isr_rcc(void);
-WEAK_DEFAULT void isr_exti(void);
-WEAK_DEFAULT void isr_dma1_channel1(void);
-WEAK_DEFAULT void isr_dma1_channel2(void);
-WEAK_DEFAULT void isr_dma1_channel3(void);
-WEAK_DEFAULT void isr_dma1_channel4(void);
-WEAK_DEFAULT void isr_dma1_channel5(void);
-WEAK_DEFAULT void isr_dma1_channel6(void);
-WEAK_DEFAULT void isr_dma1_channel7(void);
-WEAK_DEFAULT void isr_adc1_2(void);
-WEAK_DEFAULT void isr_usb_hp_can_tx(void);
-WEAK_DEFAULT void isr_usb_lp_can_rx0(void);
-WEAK_DEFAULT void isr_can_rx1(void);
-WEAK_DEFAULT void isr_can_sce(void);
-WEAK_DEFAULT void isr_tim1_brk_tim15(void);
-WEAK_DEFAULT void isr_tim1_up_tim16(void);
-WEAK_DEFAULT void isr_tim1_trg_com_tim17(void);
-WEAK_DEFAULT void isr_tim1_cc(void);
-WEAK_DEFAULT void isr_tim2(void);
-WEAK_DEFAULT void isr_tim3(void);
-WEAK_DEFAULT void isr_tim4(void);
-WEAK_DEFAULT void isr_i2c1_ev(void);
-WEAK_DEFAULT void isr_i2c1_er(void);
-WEAK_DEFAULT void isr_i2c2_ev(void);
-WEAK_DEFAULT void isr_i2c2_er(void);
-WEAK_DEFAULT void isr_spi1(void);
-WEAK_DEFAULT void isr_spi2(void);
-WEAK_DEFAULT void isr_usart1(void);
-WEAK_DEFAULT void isr_usart2(void);
-WEAK_DEFAULT void isr_usart3(void);
-WEAK_DEFAULT void isr_rtc_alarm(void);
-WEAK_DEFAULT void isr_usbwakeup(void);
-WEAK_DEFAULT void isr_tim8_brk(void);
-WEAK_DEFAULT void isr_tim8_up(void);
-WEAK_DEFAULT void isr_tim8_trg_com(void);
-WEAK_DEFAULT void isr_tim8_cc(void);
-WEAK_DEFAULT void isr_adc3(void);
-WEAK_DEFAULT void isr_spi3(void);
-WEAK_DEFAULT void isr_uart4(void);
-WEAK_DEFAULT void isr_uart5(void);
-WEAK_DEFAULT void isr_tim6_dac(void);
-WEAK_DEFAULT void isr_tim7(void);
-WEAK_DEFAULT void isr_dma2_channel1(void);
-WEAK_DEFAULT void isr_dma2_channel2(void);
-WEAK_DEFAULT void isr_dma2_channel3(void);
-WEAK_DEFAULT void isr_dma2_channel4(void);
-WEAK_DEFAULT void isr_dma2_channel5(void);
-WEAK_DEFAULT void isr_adc4(void);
-WEAK_DEFAULT void isr_comp1_2_3(void);
-WEAK_DEFAULT void isr_comp4_5_6(void);
-WEAK_DEFAULT void isr_comp7(void);
-WEAK_DEFAULT void isr_usb_hp(void);
-WEAK_DEFAULT void isr_usb_lp(void);
-WEAK_DEFAULT void isr_usbwakeup_rmp(void);
-WEAK_DEFAULT void isr_fpu(void);
+/* CC26xx specific interrupt vectors */
+WEAK_DEFAULT void GPIOIntHandler(void);
+WEAK_DEFAULT void I2CIntHandler(void);
+WEAK_DEFAULT void RFCCPE1IntHandler(void);
+WEAK_DEFAULT void AONIntHandler(void);
+WEAK_DEFAULT void AONRTCIntHandler(void);
+WEAK_DEFAULT void UART0IntHandler(void);
+WEAK_DEFAULT void AUXSWEvent0IntHandler(void);
+WEAK_DEFAULT void SSI0IntHandler(void);
+WEAK_DEFAULT void SSI1IntHandler(void);
+WEAK_DEFAULT void RFCCPE0IntHandler(void);
+WEAK_DEFAULT void RFCHardwareIntHandler(void);
+WEAK_DEFAULT void RFCCmdAckIntHandler(void);
+WEAK_DEFAULT void I2SIntHandler(void);
+WEAK_DEFAULT void AUXSWEvent1IntHandler(void);
+WEAK_DEFAULT void WatchdogIntHandler(void);
+WEAK_DEFAULT void Timer0AIntHandler(void);
+WEAK_DEFAULT void Timer0BIntHandler(void);
+WEAK_DEFAULT void Timer1AIntHandler(void);
+WEAK_DEFAULT void Timer1BIntHandler(void);
+WEAK_DEFAULT void Timer2AIntHandler(void);
+WEAK_DEFAULT void Timer2BIntHandler(void);
+WEAK_DEFAULT void Timer3AIntHandler(void);
+WEAK_DEFAULT void Timer3BIntHandler(void);
+WEAK_DEFAULT void CryptoIntHandler(void);
+WEAK_DEFAULT void uDMAIntHandler(void);
+WEAK_DEFAULT void uDMAErrIntHandler(void);
+WEAK_DEFAULT void FlashIntHandler(void);
+WEAK_DEFAULT void SWEvent0IntHandler(void);
+WEAK_DEFAULT void AUXCombEventIntHandler(void);
+WEAK_DEFAULT void AONProgIntHandler(void);
+WEAK_DEFAULT void DynProgIntHandler(void);
+WEAK_DEFAULT void AUXCompAIntHandler(void);
+WEAK_DEFAULT void AUXADCIntHandler(void);
+WEAK_DEFAULT void TRNGIntHandler(void);
 
-/* interrupt vector table */
 ISR_VECTORS const void *interrupt_vector[] = {
-    /* Exception stack pointer */
-    (void*) (&_estack),             /* pointer to the top of the stack */
-    /* Cortex-M4 handlers */
+    (void*) (&_estack),                     /* pointer to the top of the stack */
+    /* Cortex-M3 handlers */
     (void*) reset_handler_default,  /* entry point of the program */
     (void*) nmi_default,            /* non maskable interrupt handler */
     (void*) hard_fault_default,     /* hard fault exception */
@@ -118,87 +108,41 @@ ISR_VECTORS const void *interrupt_vector[] = {
     (void*) isr_pendsv,             /* pendSV interrupt, in RIOT the actual
                                      * context switching is happening here */
     (void*) isr_systick,            /* SysTick interrupt, not used in RIOT */
-    /* STMF3 specific peripheral handlers */
-    (void*) isr_wwdg,               /*  */
-    (void*) isr_pvd,                /*  */
-    (void*) isr_tamp_stamp,         /*  */
-    (void*) isr_rtc_wkup,           /*  */
-    (void*) isr_flash,              /*  */
-    (void*) isr_rcc,                /*  */
-    (void*) isr_exti,               /*  */
-    (void*) isr_exti,               /*  */
-    (void*) isr_exti,               /*  */
-    (void*) isr_exti,               /*  */
-    (void*) isr_exti,               /*  */
-    (void*) isr_dma1_channel1,      /*  */
-    (void*) isr_dma1_channel2,      /*  */
-    (void*) isr_dma1_channel3,      /*  */
-    (void*) isr_dma1_channel4,      /*  */
-    (void*) isr_dma1_channel5,      /*  */
-    (void*) isr_dma1_channel6,      /*  */
-    (void*) isr_dma1_channel7,      /*  */
-    (void*) isr_adc1_2,             /*  */
-    (void*) isr_usb_hp_can_tx,      /*  */
-    (void*) isr_usb_lp_can_rx0,     /*  */
-    (void*) isr_can_rx1,            /*  */
-    (void*) isr_can_sce,            /*  */
-    (void*) isr_exti,               /*  */
-    (void*) isr_tim1_brk_tim15,     /*  */
-    (void*) isr_tim1_up_tim16,      /*  */
-    (void*) isr_tim1_trg_com_tim17, /*  */
-    (void*) isr_tim1_cc,            /*  */
-    (void*) isr_tim2,               /*  */
-    (void*) isr_tim3,               /*  */
-    (void*) isr_tim4,               /*  */
-    (void*) isr_i2c1_ev,            /*  */
-    (void*) isr_i2c1_er,            /*  */
-    (void*) isr_i2c2_ev,            /*  */
-    (void*) isr_i2c2_er,            /*  */
-    (void*) isr_spi1,               /*  */
-    (void*) isr_spi2,               /*  */
-    (void*) isr_usart1,             /*  */
-    (void*) isr_usart2,             /*  */
-    (void*) isr_usart3,             /*  */
-    (void*) isr_exti,               /*  */
-    (void*) isr_rtc_alarm,          /*  */
-    (void*) isr_usbwakeup,          /*  */
-    (void*) isr_tim8_brk,           /*  */
-    (void*) isr_tim8_up,            /*  */
-    (void*) isr_tim8_trg_com,       /*  */
-    (void*) isr_tim8_cc,            /*  */
-    (void*) isr_adc3,               /*  */
-    (void*) (0UL),                  /* reserved */
-    (void*) (0UL),                  /* reserved */
-    (void*) (0UL),                  /* reserved */
-    (void*) isr_spi3,               /*  */
-    (void*) isr_uart4,              /*  */
-    (void*) isr_uart5,              /*  */
-    (void*) isr_tim6_dac,           /*  */
-    (void*) isr_tim7,               /*  */
-    (void*) isr_dma2_channel1,      /*  */
-    (void*) isr_dma2_channel2,      /*  */
-    (void*) isr_dma2_channel3,      /*  */
-    (void*) isr_dma2_channel4,      /*  */
-    (void*) isr_dma2_channel5,      /*  */
-    (void*) isr_adc4,               /*  */
-    (void*) (0UL),                  /* reserved */
-    (void*) (0UL),                  /* reserved */
-    (void*) isr_comp1_2_3,          /*  */
-    (void*) isr_comp4_5_6,          /*  */
-    (void*) isr_comp7,              /*  */
-    (void*) (0UL),                  /* reserved */
-    (void*) (0UL),                  /* reserved */
-    (void*) (0UL),                  /* reserved */
-    (void*) (0UL),                  /* reserved */
-    (void*) (0UL),                  /* reserved */
-    (void*) (0UL),                  /* reserved */
-    (void*) (0UL),                  /* reserved */
-    (void*) isr_usb_hp,             /*  */
-    (void*) isr_usb_lp,             /*  */
-    (void*) isr_usbwakeup_rmp,      /*  */
-    (void*) (0UL),                  /* reserved */
-    (void*) (0UL),                  /* reserved */
-    (void*) (0UL),                  /* reserved */
-    (void*) (0UL),                  /* reserved */
-    (void*) isr_fpu,                /*  */
+    /* CC26xx specific peripheral handlers */
+    (void*) GPIOIntHandler,                         // AON edge detect
+    (void*) I2CIntHandler,                          // I2C
+    (void*) RFCCPE1IntHandler,                      // RF Core Command & Packet Engine 1
+    (void*) AONIntHandler,                          // AON SpiSplave Rx, Tx and CS
+    (void*) AONRTCIntHandler,                       // AON RTC
+    (void*) UART0IntHandler,                        // UART0 Rx and Tx
+    (void*) AUXSWEvent0IntHandler,                  // AUX software event 0
+    (void*) SSI0IntHandler,                         // SSI0 Rx and Tx
+    (void*) SSI1IntHandler,                         // SSI1 Rx and Tx
+    (void*) RFCCPE0IntHandler,                      // RF Core Command & Packet Engine 0
+    (void*) RFCHardwareIntHandler,                  // RF Core Hardware
+    (void*) RFCCmdAckIntHandler,                    // RF Core Command Acknowledge
+    (void*) I2SIntHandler,                          // I2S
+    (void*) AUXSWEvent1IntHandler,                  // AUX software event 1
+    (void*) WatchdogIntHandler,                     // Watchdog timer
+    (void*) Timer0AIntHandler,                      // Timer 0 subtimer A
+    (void*) Timer0BIntHandler,                      // Timer 0 subtimer B
+    (void*) Timer1AIntHandler,                      // Timer 1 subtimer A
+    (void*) Timer1BIntHandler,                      // Timer 1 subtimer B
+    (void*) Timer2AIntHandler,                      // Timer 2 subtimer A
+    (void*) Timer2BIntHandler,                      // Timer 2 subtimer B
+    (void*) Timer3AIntHandler,                      // Timer 3 subtimer A
+    (void*) Timer3BIntHandler,                      // Timer 3 subtimer B
+    (void*) CryptoIntHandler,                       // Crypto Core Result available
+    (void*) uDMAIntHandler,                         // uDMA Software
+    (void*) uDMAErrIntHandler,                      // uDMA Error
+    (void*) FlashIntHandler,                        // Flash controller
+    (void*) SWEvent0IntHandler,                     // Software Event 0
+    (void*) AUXCombEventIntHandler,                 // AUX combined event
+    (void*) AONProgIntHandler,                      // AON programmable 0
+    (void*) DynProgIntHandler,                      // Dynamic Programmable interrupt
+                                                    // source (Default: PRCM)
+    (void*) AUXCompAIntHandler,                     // AUX Comparator A
+    (void*) AUXADCIntHandler,                       // AUX ADC new sample or ADC DMA
+                                                    // done, ADC underflow, ADC overflow
+    (void*) TRNGIntHandler                          // TRNG event
 };
