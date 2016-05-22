46041fca697ac897128c5be2e396f601d11c4b76
Thomas Stilwell <stilwellt@openlabs.co>
2016-05-22 03:22:52 -0500

    test bootloader flash writing; works

--- cpu/cc26xx-cc13xx_common/periph/flash.c
+++ cpu/cc26xx-cc13xx_common/periph/flash.c
@@ -27,29 +27,34 @@
 #include <string.h>
 #include <stdio.h>
 #include "driverlib/flash.h"
-#include "driverlib/vims.h"
-#include "xtimer.h"
+#include "cpu.h"
 
-__attribute__((used,section(".boot"))) int flash_read(uint32_t read_address, uint8_t *buffer, uint32_t length) {
+BOOT_FUNC void _memcpy(void *dest, const void *src, size_t size) {
+    uint word_size = 4;
+    for (int i = 0; i < size; ) {
+        *(uint32_t*)dest = *(uint32_t*)src;
+        i += word_size;
+        dest += word_size;
+        src += word_size;
+    }
+}
+
+BOOT_FUNC int flash_read(uint32_t read_address, uint8_t *buffer, uint32_t length) {
     memcpy(buffer, (uint32_t*)read_address, length);
     return 0;
 }
 
-__attribute__((used,section(".boot"))) int flash_write(uint32_t write_address, uint8_t *buffer, uint32_t length) {
+BOOT_FUNC int flash_write(uint32_t write_address, uint8_t *buffer, uint32_t length) {
     return 0;
 }
-__attribute__((used,section(".boot"))) int flash_copy_sectors(uint32_t read_address, uint32_t write_address, uint32_t bytes)
+BOOT_FUNC int flash_copy_sectors(uint32_t read_address, uint32_t write_address, uint32_t bytes)
 {
     /* write n chunks of size sizeof(buf), erasing sectors as necessary */
-    uint8_t buf[512];
+    uint8_t buf[128];
     uint32_t sector_size = 4096;
     uint32_t ret = 0;
     uint32_t last_erased_address = 0xffffffff;
 
-//     PRCMCacheRetentionDisable();
-//     VIMSModeSet(VIMS_BASE, VIMS_MODE_OFF);
-
-//     IntMasterDisable();
     __disable_irq();
 
     while(bytes) {
@@ -58,22 +63,16 @@ __attribute__((used,section(".boot"))) int flash_copy_sectors(uint32_t read_addr
         if (erase_address + sector_size == 0x20000) {
             /* never erase the last flash sector, as the default ccfg values
              * subsequently applied by ti-lib apparently disallow jtag access */
-//             printf("NOT ERASING LAST SECTOR from 0x%lx to 0x%lx\n",
-//                    erase_address, erase_address + sector_size);
             ret = -1;
             goto out;
         } else if (erase_address < 0x1000) {
             /* never erase the bootloader sector */
-//             printf("NOT ERASING BOOTLOADER SECTOR from 0x%lx to 0x%lx\n",
-//                    erase_address, erase_address + sector_size);
             ret = -1;
             goto out;
         }
 
         /* erase sector if not yet erased */
         if (erase_address != last_erased_address) {
-//             printf("erasing sector from 0x%lx to 0x%lx\n",
-//                    erase_address, erase_address + sector_size);
             last_erased_address = erase_address;
             ret = FlashSectorErase(erase_address);
             if (ret) {
@@ -85,9 +84,7 @@ __attribute__((used,section(".boot"))) int flash_copy_sectors(uint32_t read_addr
         uint32_t chunk_length = sizeof(buf);
         if (chunk_length > bytes)
             chunk_length = bytes;
-        memcpy(buf, (uint32_t*)read_address, chunk_length);
-//         printf("flashing %li bytes from 0x%lx to 0x%lx\n",
-//                chunk_length, write_address, write_address + chunk_length);
+        _memcpy(buf, (uint32_t*)read_address, chunk_length);
 
         ret = FlashProgram(buf, write_address, chunk_length);
         if (ret) {
@@ -99,12 +96,11 @@ __attribute__((used,section(".boot"))) int flash_copy_sectors(uint32_t read_addr
     }
 
     out:
-//     IntMasterEnable();
     __enable_irq();
     return ret;
 }
 
-__attribute__((used,section(".boot"))) int copy_test(void)
+BOOT_FUNC int flash_copy_test(void)
 {
     uint32_t low_begin = 0x1000;
     uint32_t high_begin = 0x10000;
@@ -112,9 +108,9 @@ __attribute__((used,section(".boot"))) int copy_test(void)
     int ret = 0;
 
     ret = flash_copy_sectors(low_begin, high_begin, size);
-    if (ret) goto out;
+    if (ret) return ret;
+
     ret = flash_copy_sectors(high_begin, low_begin, size);
 
-    out:
     return ret;
 }
