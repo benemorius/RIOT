47ba2adfe0fdf4f3873881ce6fe3e6589b50cdca
Thomas Stilwell <stilwellt@openlabs.co>
2016-05-22 01:32:55 -0500

    implement flash writing functions for ota firmware upgrade

---
+++ cpu/cc26xx-cc13xx_common/include/flash.h
@@ -0,0 +1,40 @@
+/*
+ *    Copyright (c) 2016 Thomas Stilwell <stilwellt@openlabs.co>
+ *
+ *    Permission is hereby granted, free of charge, to any person
+ *    obtaining a copy of this software and associated documentation
+ *    files (the "Software"), to deal in the Software without
+ *    restriction, including without limitation the rights to use,
+ *    copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *    copies of the Software, and to permit persons to whom the
+ *    Software is furnished to do so, subject to the following
+ *    conditions:
+ *
+ *    The above copyright notice and this permission notice shall be
+ *    included in all copies or substantial portions of the Software.
+ *
+ *    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ *    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ *    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ *    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ *    OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include "sys/types.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+int flash_read(uint32_t read_address, uint8_t *buffer, uint32_t length);
+int flash_write(uint32_t write_address, uint8_t *buffer, uint32_t length);
+int flash_copy_sectors(uint32_t read_address, uint32_t write_address, uint32_t bytes);
+int copy_test(void);
+
+#ifdef __cplusplus
+}
+#endif
