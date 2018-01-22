d5912f8b0490995b03f103bbdf48f3e4dfabd4de
Thomas Stilwell <stilwellt@openlabs.co>
2018-01-22 15:26:02 -0600

    add support for olimex arm-usb-ocd

---
+++ makefiles/tools/openocd-adapters/olimex.inc.mk
@@ -0,0 +1,10 @@
+# Olimex ARM-USB-OCD JTAG interface with SWD adapter
+OPENOCD_ADAPTER_INIT ?= \
+  -c 'source [find interface/ftdi/olimex-arm-usb-ocd.cfg]' \
+  -c 'source [find interface/ftdi/olimex-arm-jtag-swd.cfg]'
+
+# Add serial matching command, only if DEBUG_ADAPTER_ID was specified
+ifneq (,$(DEBUG_ADAPTER_ID))
+  OPENOCD_ADAPTER_INIT += -c 'ftdi_serial $(DEBUG_ADAPTER_ID)'
+endif
+export OPENOCD_ADAPTER_INIT
