7ae8e0e74bf83ee588def1778f6bac66f2a9e6e5
Thomas Stilwell <stilwellt@openlabs.co>
2019-01-03 02:47:37 -0800

    dmesg fixup

--- sys/include/log_dmesg.h
+++ sys/include/log_dmesg.h
@@ -1,1 +1,9 @@
+#include <sys/types.h>
+
 void log_dmesg(unsigned level, const char *format, ...);
+size_t dmesg_peek(char *buffer, size_t buffer_length, size_t offset);
+size_t dmesg_read(char *buffer, size_t buffer_length);
+size_t dmesg_free(void);
+size_t dmesg_total(void);
+size_t dmesg_used(void);
+void dmesg_clear(void);
