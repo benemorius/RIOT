7a3e6ad14da194903c9df25b8cfd9ece31c968dd
Thomas Stilwell <stilwellt@openlabs.co>
2019-01-04 04:56:07 -0800

    sys/shell: add single-depth command history

--- sys/shell/shell.c
+++ sys/shell/shell.c
@@ -46,6 +46,15 @@ static void _putchar(int c) {
 #endif
 #endif
 
+#define LINE_HISTORY_COUNT (2)
+#define LINE_HISTORY_LENGTH (SHELL_DEFAULT_BUFSIZE)
+static char line_history[LINE_HISTORY_COUNT][LINE_HISTORY_LENGTH];
+static unsigned line_history_index;
+
+static inline void print_prompt(void);
+static void replace_line(const char *line_buf);
+static void refresh_line(const char *line_buf);
+
 static shell_command_handler_t find_handler(const shell_command_t *command_list, char *command)
 {
     const shell_command_t *command_lists[] = {
@@ -222,12 +231,9 @@ static void handle_input_line(const shell_command_t *command_list, char *line)
 static int readline(char *buf, size_t size)
 {
     char *line_buf_ptr = buf;
+    *line_buf_ptr = '\0';
 
     while (1) {
-        if ((line_buf_ptr - buf) >= ((int) size) - 1) {
-            return -1;
-        }
-
         int c = getchar();
         if (c < 0) {
             return EOF;
@@ -237,14 +243,13 @@ static int readline(char *buf, size_t size)
         /* QEMU transmits only a single '\r' == 13 on hitting enter ("-serial stdio"). */
         /* DOS newlines are handled like hitting enter twice, but empty lines are ignored. */
         if (c == '\r' || c == '\n') {
-            *line_buf_ptr = '\0';
 #ifndef SHELL_NO_ECHO
             _putchar('\r');
             _putchar('\n');
 #endif
 
             /* return 1 if line is empty, 0 otherwise */
-            return line_buf_ptr == buf;
+            return buf[0] == '\0';
         }
         /* ^C */
         else if (c == 'C' - '@') {
@@ -262,7 +267,23 @@ static int readline(char *buf, size_t size)
                 continue;
             }
 
-            *--line_buf_ptr = '\0';
+            /* overwrite previous character and keep shifting left until end */
+            --line_buf_ptr;
+            char *line_buf_ptr_restore = line_buf_ptr;
+            while (line_buf_ptr[1] != '\0') {
+                *line_buf_ptr = line_buf_ptr[1];
+                ++line_buf_ptr;
+            }
+            *line_buf_ptr = '\0';
+
+            /* if deleted a character somewhere in the middle of the line */
+            if (line_buf_ptr != line_buf_ptr_restore) {
+                fputs("\033[D", stdout); /* move cursor left one */
+                refresh_line(buf);
+                line_buf_ptr = line_buf_ptr_restore;
+                continue;
+            }
+
             /* white-tape the character */
 #ifndef SHELL_NO_ECHO
             _putchar('\b');
@@ -276,17 +297,76 @@ static int readline(char *buf, size_t size)
                 continue;
             }
             c = getchar();
-            /* up down right left */
-            if (c == 'A' || c == 'B' || c == 'C' || c == 'D') {
-                /* do nothing; mostly just don't echo */
+            /* up or down */
+            if (c == 'A' || c == 'B') {
+                if (c == 'A' && line_history_index < LINE_HISTORY_COUNT - 1) {
+                    strcpy(line_history[0], buf);
+                    strcpy(buf, line_history[1]);
+                    line_buf_ptr = buf + strlen(buf);
+                    replace_line(buf);
+                    line_history_index = 1;
+                }
+                if (c == 'B' && line_history_index > 0) {
+                    strcpy(line_history[1], buf);
+                    strcpy(buf, line_history[0]);
+                    line_buf_ptr = buf + strlen(buf);
+                    replace_line(buf);
+                    line_history_index = 0;
+                }
+            }
+            /* left or right */
+            else if (c == 'D' || c == 'C') {
+                int delta = c == 'D' ? -1 : 1;
+                if (delta > 0 && *line_buf_ptr == '\0') {
+                    continue;
+                }
+                if (delta < 0 && line_buf_ptr == buf) {
+                    continue;
+                }
+                line_buf_ptr += delta;
+                if (delta > 0) {
+                    fputs("\033[C", stdout);
+                }
+                else {
+                    fputs("\033[D", stdout);
+                }
             }
             else {
                 const char msg[] = "[shell] unhandled escape sequence";
                 LOG_INFO("%s ^[[<0x%02x> (\\033[\\0%o)\n", msg, c, c);
             }
         }
+        /* received new character to add to line buffer */
         else {
-            *line_buf_ptr++ = c;
+            /* can't add another character if line buffer is full */
+            if ((line_buf_ptr - buf) >= ((int) size) - 1) {
+                continue;
+            }
+
+            /* check if we're inserting somewhere before the end of the string,
+             * and make room for the inserted character if so
+             */
+            if (*line_buf_ptr != '\0') {
+                /* shift right one by one until string ends */
+                char tmp;
+                char *line_buf_ptr_restore = line_buf_ptr;
+                while (*line_buf_ptr != '\0') {
+                    tmp = c;
+                    c = *line_buf_ptr;
+                    *line_buf_ptr++ = tmp;
+                }
+                *line_buf_ptr++ = c;
+                *line_buf_ptr = '\0';
+                line_buf_ptr = line_buf_ptr_restore + 1;
+                /* move cursor right one */
+                fputs("\033[C", stdout);
+                refresh_line(buf);
+                continue;
+            }
+            else {
+                *line_buf_ptr++ = c;
+                *line_buf_ptr = '\0';
+            }
 #ifndef SHELL_NO_ECHO
             _putchar(c);
 #endif
@@ -295,6 +375,26 @@ static int readline(char *buf, size_t size)
         fflush(stdout);
 #endif
     }
+}
+
+static void replace_line(const char *line_buf)
+{
+   /* delete line */
+    fputs("\033[2K\r", stdout);
+    print_prompt();
+    fputs(line_buf, stdout);
+    fflush(stdout);
+}
+
+static void refresh_line(const char *line_buf)
+{
+   /* store cursor location and delete line */
+    fputs("\033[s\033[2K\r", stdout);
+    print_prompt();
+    fputs(line_buf, stdout);
+    /* restore cursor location */
+    fputs("\033[u", stdout);
+    fflush(stdout);
 }
 
 static inline void print_prompt(void)
@@ -311,6 +411,7 @@ static inline void print_prompt(void)
 
 void shell_run(const shell_command_t *shell_commands, char *line_buf, int len)
 {
+    /* begin prompt on a new line */
     _putchar('\r');
     _putchar('\n');
     print_prompt();
@@ -323,6 +424,12 @@ void shell_run(const shell_command_t *shell_commands, char *line_buf, int len)
         }
 
         if (!res) {
+            /* add command to history if it doesn't match the last command */
+            // if (strcmp(line_buf, line_history[line_history_index - 1])) {
+            if (1) {
+                strcpy(line_history[1], line_buf);
+                line_history_index = 0;
+            }
             handle_input_line(shell_commands, line_buf);
         }
 
